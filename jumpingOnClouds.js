68ffebea711e5f280ac0c3091c8a0c81de01f41b
Dmitriy "dmitriyshin.io" Shin <dimashin9@gmail.com>
2020-04-08 10:58:16 -0400

    Solved jumpingOnClouds

--- jumpingOnClouds.js
+++ jumpingOnClouds.js
@@ -8,19 +8,18 @@
  */
 
 const jumpingOnClouds = (c = []) => {
-  const firstPossibleJumps = c
-    .map((cloud, index) => (cloud !== 1 ? index : null))
-    .filter((cloud) => cloud !== null).length
-
-  let currentMinJumps = firstPossibleJumps
-  const start = 0 // Index of the starting point
-  const end = c.length - 1 // Index of the end point
-
-  for () {}
-
-  return 
+  let minJumps = 0
+  for (let index = 0; index < c.length; ) {
+    if (c[index] === 0 && c[index + 2] === 0) {
+      minJumps++
+      index += 2
+    } else if (c[index] === 0 && c[index + 1] === 0 && c[index + 2] !== 0) {
+      minJumps++
+      index++
+    } else break
+  }
+  return minJumps
 }
 
 console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0])) // -> 4
-// console.log(jumpingOnClouds([0, 0, 0, 0, 1, 0])) // -> 3
-// console.log(jumpingOnClouds([0, 1, 0, 0, 0, 1, 0])) // -> 4
+console.log(jumpingOnClouds([0, 0, 0, 0, 1, 0])) // -> 3
