91bc72054ab150eef7ec677898456727c4c2ce24
Dmitiry Shin <dimashin9@gmail.com>
2020-05-13 12:37:12 -0400

    Finished activity notifications using Python3

---
+++ Sorting/Activity Notifications/activityNotifications.py
@@ -0,0 +1,10 @@
+def activityNotifications(expenditure, d):
+    arr = sorted(expenditure[0:d])
+    m = d // 2
+    c = 0
+    for i in range(d, len(expenditure)):
+      if expenditure[i] >= 2 * med(arr, d, m):
+        c += 1
+      del arr[bisect.bisect_left(arr, expenditure[i-d])]
+      bisect.insort(arr, expenditure[i])
+    return c
