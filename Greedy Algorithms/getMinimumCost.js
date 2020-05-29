131d937bc86da31ec1877516b6da69b68f0b1b9f
Dmitiry Shin <dimashin9@gmail.com>
2020-05-29 11:20:02 -0400

    Solved Greedy Florist

--- Greedy Algorithms/getMinimumCost.js
+++ Greedy Algorithms/getMinimumCost.js
@@ -8,18 +8,22 @@
 const fs = require('fs')
 
 const getMinimumCost = (c = [], k = 0) => {
-	const reversed = [ ...c ].reverse()
+	const sorted = [ ...c ].sort((a, b) => (a > b ? 1 : -1)).reverse()
+	console.log(sorted)
 
-	let total = reversed.slice(0, k).reduce((a, b) => a + b, 0)
-	const left = reversed.slice(k)
+	let prevPurchase = -1
+	let minimumCost = 0
 
-	if (left.length) {
-		for (let i = 0; i < left.length; i++) {
-			total += (1 + 1) * left[i]
+	for (let i = 0; i < sorted.length; i++) {
+		if (i % k === 0) {
+			prevPurchase++
+			console.log(i, k, sorted[i])
 		}
+		minimumCost += sorted[i] * (prevPurchase + 1)
+		console.log(minimumCost, prevPurchase)
 	}
 
-	return total
+	return minimumCost
 }
 
 console.log(getMinimumCost([ 2, 5, 6 ], 3)) // 13
@@ -28,7 +32,6 @@ console.log(getMinimumCost([ 1, 3, 5, 7, 9 ], 3)) // 29
 
 fs.readFile('./bigtest1.txt', 'utf-8', (error, data) => {
 	if (error) throw new Error(error)
-
 	const v = data.split(' ').map((n) => +n)
 	console.log(getMinimumCost(v, 3)) // 163578911
 })
