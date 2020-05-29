54707bb4a0616d9adc1edc5a1ca46d87502210cb
Dmitiry Shin <dimashin9@gmail.com>
2020-05-29 11:23:43 -0400

    Added Max Min

---
+++ Greedy Algorithms/maxMin.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * Author: Dmitriy Shin
+ * Date: May 29, 2020
+ * Program name: Max Min
+ * 
+ */
+
+const maxMin = (k = 0, arr = []) => {}
+
+console.log(maxMin(3, [ 10, 100, 300, 200, 1000, 20, 30 ])) // 20
+console.log(maxMin(4, [ 1, 2, 3, 4, 10, 20, 30, 40, 100, 200 ])) // 3
+console.log(maxMin(2, [ 1, 2, 1, 2, 1 ])) // 0
